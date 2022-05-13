b0484ae6a812b958c2693864715e21c8b305d3a4
alestucci <alessandrotucci@gmail.com>
2022-05-13 17:16:20 +0200

    Destroy

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -11,11 +11,13 @@
                 <div class="card-body">
                     <h1 class="text-center">{{ $comic->title }}</h1>
                     <h6 class="text-center text-uppercase text-success">{{ $comic->type }}</h6>
-                    <h6 class="text-center text-primary">Serie: <span class="fst-italic">{{ $comic->series }}</span></h6>
+                    <h6 class="text-center text-primary">Serie: <span class="fst-italic">{{ $comic->series }}</span>
+                    </h6>
                     <p>{{ $comic->description }}</p>
                     <div class="row justify-content-between">
                         <div class="col-6">
-                            <button class="btn btn-info">Disponibile dal {{date('d/m/Y', strtotime($comic->sale_date))}}</button>
+                            <button class="btn btn-info">Disponibile dal {{date('d/m/Y',
+                                strtotime($comic->sale_date))}}</button>
                         </div>
                         <div class="col-3 d-flex justify-content-end">
                             <button class="btn btn-secondary">€ {{$comic->price }}</button>
@@ -23,6 +25,11 @@
                     </div>
                     <div class="d-flex justify-content-end">
                         <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-light">Edit</a>
+                        <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Cancella</button>
+                        </form>
                     </div>
                 </div>
             </div>
